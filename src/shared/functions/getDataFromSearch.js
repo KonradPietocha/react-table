49d117a988643defc8d1fd458c61b95f5ef5a524
KonradPietocha <konrad.pietocha@gazeta.pl>
2020-09-20 23:29:20 +0200

    change value from search input to lower case

--- src/shared/functions/getDataFromSearch.js
+++ src/shared/functions/getDataFromSearch.js
@@ -1,6 +1,6 @@
 export const getDataFromSearch = (searchText, arrayForTable) => {
     const result = arrayForTable.filter(o =>
         Object.keys(o).some(k =>
-            o[k].toString().toLowerCase().indexOf(searchText) !== -1));
+            o[k].toString().toLowerCase().indexOf(searchText.toLowerCase()) !== -1));
     return result;
 };
