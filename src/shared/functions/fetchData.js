0f64a0c673e87a5a82a06ae5dce24db2cda2fc1c
KonradPietocha <konrad.pietocha@gazeta.pl>
2022-10-18 12:24:43 +0200

    Update url and add key and demo data

--- src/shared/functions/fetchData.js
+++ src/shared/functions/fetchData.js
@@ -1,16 +1,29 @@
+import { demoData } from "../demo";
+import { API_KEY } from '../urls';
+
 export const fetchData = (url, dealWithTheResult, setState) => {
-    fetch(url, { method: "GET" })
+    fetch(url, {
+        method: "GET",
+        headers: {
+            apikey: API_KEY
+        }
+    })
         .then(data => {
             if (data.ok) {
                 const response = data.json();
                 response.then(result => {
-                    dealWithTheResult(result, setState);
+                    if (!result.success) {
+                        dealWithTheResult(demoData, setState);
+                    } else {
+                        dealWithTheResult(result, setState);
+                    }
                 });
             } else {
-                alert("Wystąpił błąd. Przepraszamy");
+                dealWithTheResult(demoData, setState);
             }
         })
         .catch(err => {
-            console.error(err);
+            console.error('TEST', err);
+            dealWithTheResult(demoData, setState);
         });
 };
--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,4 +1,6 @@
-export const urlCurrencyLatest = "https://api.exchangeratesapi.io/latest";
-export const urlCurrencyLatestPair = "https://api.exchangeratesapi.io/latest?symbols=";
-export const urlCurrencyLatestBase = "https://api.exchangeratesapi.io/latest?base=";
-export const urlCurrencyHistoryPeriod = "https://api.exchangeratesapi.io/history?";
+export const API_KEY = "1BixjhzFKYDVSQj2MqTS2XkplEsqjklG";
+
+export const urlCurrencyLatest = "https://api.apilayer.com/exchangerates_data/latest";
+export const urlCurrencyLatestPair = "https://api.apilayer.com/exchangerates_data/latest?symbols=";
+export const urlCurrencyLatestBase = "https://api.apilayer.com/exchangerates_data/latest?base=";
+export const urlCurrencyHistoryPeriod = "https://api.apilayer.com/exchangerates_data/history?";
