28a80d076884ed0e477e3d81e052cb1b679b3ee8
KonradPietocha <konrad.pietocha@gazeta.pl>
2026-04-03 12:32:40 +0200

    Additional information added

--- src/components/CurrencyPage.jsx
+++ src/components/CurrencyPage.jsx
@@ -1,49 +1,49 @@
-import React from 'react';
+import React from "react";
 //components
-import Table from './Table/Table';
+import Table from "./Table/Table";
 //style
 const style = {
-    paragraph: {
-        textAlign: "center"
-    }
+  paragraph: {
+    textAlign: "center",
+  },
 };
 
 const mapDataToCurrencyArray = (data) => {
-    return data
-        ? Object.entries(data).map(([key, value]) => ({ currencyCode: key, value: value }))
-        : [];
-}
+  return data
+    ? Object.entries(data).map(([key, value]) => ({
+        currencyCode: key,
+        value: value,
+      }))
+    : [];
+};
 
 function CurrencyPage(props) {
-    const {
-        state,
-        handleSorting,
-        handlePagination,
-        handleSearch,
-        handleSelect
-    } = props;
+  const { state, handleSorting, handlePagination, handleSearch, handleSelect } =
+    props;
 
-    return (
-        <>
-            <h1>Currency table</h1>
-            {state.data.isDemo
-                ? <p style={style.paragraph}>
-                    Oops, something went wrong with the download. The presented data is a demo.
-                </p>
-                : <p style={style.paragraph}>
-                    The latest data for the {state.data.base} currency.
-                </p>
-            }
-            <Table
-                arrayForTable={mapDataToCurrencyArray(state.data.rates)}
-                state={state}
-                handleSorting={handleSorting}
-                handlePagination={handlePagination}
-                handleSearch={handleSearch}
-                handleSelect={handleSelect}
-            />
-        </>
-    );
+  return (
+    <>
+      <h1>Currency table</h1>
+      {state.data.isDemo ? (
+        <p style={style.paragraph}>
+          Oops, something went wrong with the download. The presented data is a
+          demo.
+        </p>
+      ) : (
+        <p style={style.paragraph}>
+          The latest data for the {state.data.base} currency.
+        </p>
+      )}
+      <Table
+        arrayForTable={mapDataToCurrencyArray(state.data.rates)}
+        state={state}
+        handleSorting={handleSorting}
+        handlePagination={handlePagination}
+        handleSearch={handleSearch}
+        handleSelect={handleSelect}
+      />
+    </>
+  );
 }
 
 export default CurrencyPage;
--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,97 +1,100 @@
-import React from 'react';
+import React from "react";
 //components
-import TableHeader from '../TableHeaders/TableHeaders';
-import PaginationFooter from '../PaginationFooter/PaginationFooter';
-import SearchInput from '../SearchInput/SearchInput';
+import TableHeader from "../TableHeaders/TableHeaders";
+import PaginationFooter from "../PaginationFooter/PaginationFooter";
+import SearchInput from "../SearchInput/SearchInput";
 //functions
-import { filterDataBySearchValue } from '../../shared/functions/filterDataBySearchValue';
-import SelectInput from '../SelectInput/SelectInput';
+import { filterDataBySearchValue } from "../../shared/functions/filterDataBySearchValue";
+import SelectInput from "../SelectInput/SelectInput";
 //style
 const style = {
-    table: {
-        width: "90vw"
-    },
-    tableCell: {
-        border: "1px solid black",
-        fontSize: "20px"
-    },
-    nonNumCell: {
-        textAlign: "center"
-    },
-    numCell: {
-        textAlign: "end",
-        paddingRight: "15px"
-    }
+  table: {
+    width: "90vw",
+  },
+  tableCell: {
+    border: "1px solid black",
+    fontSize: "20px",
+  },
+  nonNumCell: {
+    textAlign: "center",
+  },
+  numCell: {
+    textAlign: "end",
+    paddingRight: "15px",
+  },
 };
 
 export default function Table(props) {
-    const { arrayForTable, state, handleSorting, handlePagination, handleSearch, handleSelect } = props;
+  const {
+    arrayForTable,
+    state,
+    handleSorting,
+    handlePagination,
+    handleSearch,
+    handleSelect,
+  } = props;
 
-    const rowsEnd = state.rowsPerPage * state.page;
-    const rowsStart = rowsEnd - state.rowsPerPage;
-    const processedArray = filterDataBySearchValue(state, arrayForTable);
+  const rowsEnd = state.rowsPerPage * state.page;
+  const rowsStart = rowsEnd - state.rowsPerPage;
+  const processedArray = filterDataBySearchValue(state, arrayForTable);
 
-    return (
-        <>
-            <SelectInput
-                data={state.data}
-                isLoading={state.isLoading}
-                handleSelect={handleSelect}
-            />
-            <SearchInput
-                handleSearch={handleSearch}
-            />
-            <table style={style.table}>
-                <thead>
-                    {state.mobileMode ?
-                        <tr>
-                            <th>Date: {state.data.date}</th>
-                            <th>Base: {state.data.base}</th>
-                        </tr> : null
-                    }
-                    <tr>
-                        <TableHeader
-                            handleSorting={handleSorting}
-                            state={state}
-                        />
-                    </tr>
-                </thead>
-                <tbody>
-                    {processedArray.length
-                        ? processedArray.slice(rowsStart, rowsEnd).map((value, key) => (
-                            <tr key={key}>
-                                {state.mobileMode ? null :
-                                    <td style={{ ...style.nonNumCell, ...style.tableCell }}>
-                                        {state.data.date}
-                                    </td>
-                                }
-                                {state.mobileMode ? null :
-                                    <td style={{ ...style.nonNumCell, ...style.tableCell }}>
-                                        {state.data.base}
-                                    </td>
-                                }
-                                <td style={{ ...style.nonNumCell, ...style.tableCell }}>
-                                    {value.currencyCode}
-                                </td>
-                                <td style={{ ...style.numCell, ...style.tableCell }}>
-                                    {value.value.toFixed(2)}
-                                </td>
-                            </tr>
-                        ))
-                        : <tr>
-                            <td style={style.nonNumCell}
-                                colSpan={state.mobileMode ? 2 : 4}>
-                                <b>Loading...</b>
-                            </td>
-                        </tr>
-                    }
-                </tbody>
-            </table>
-            <PaginationFooter
-                handlePagination={handlePagination}
-                state={state}
-                pageLimit={Math.ceil(processedArray.length / 10)}
-            />
-        </>
-    );
+  return (
+    <>
+      <SelectInput
+        data={state.data}
+        isLoading={state.isLoading}
+        handleSelect={handleSelect}
+      />
+      <SearchInput handleSearch={handleSearch} />
+      <p>Click on column headers to sort the data.</p>
+      <table style={style.table}>
+        <thead>
+          {state.mobileMode ? (
+            <tr>
+              <th>Date: {state.data.date}</th>
+              <th>Base: {state.data.base}</th>
+            </tr>
+          ) : null}
+          <tr>
+            <TableHeader handleSorting={handleSorting} state={state} />
+          </tr>
+        </thead>
+        <tbody>
+          {processedArray.length ? (
+            processedArray.slice(rowsStart, rowsEnd).map((value, key) => (
+              <tr key={key}>
+                {state.mobileMode ? null : (
+                  <td style={{ ...style.nonNumCell, ...style.tableCell }}>
+                    {state.data.date}
+                  </td>
+                )}
+                {state.mobileMode ? null : (
+                  <td style={{ ...style.nonNumCell, ...style.tableCell }}>
+                    {state.data.base}
+                  </td>
+                )}
+                <td style={{ ...style.nonNumCell, ...style.tableCell }}>
+                  {value.currencyCode}
+                </td>
+                <td style={{ ...style.numCell, ...style.tableCell }}>
+                  {value.value.toFixed(2)}
+                </td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td style={style.nonNumCell} colSpan={state.mobileMode ? 2 : 4}>
+                <b>Loading...</b>
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+      <PaginationFooter
+        handlePagination={handlePagination}
+        state={state}
+        pageLimit={Math.ceil(processedArray.length / 10)}
+      />
+    </>
+  );
 }
